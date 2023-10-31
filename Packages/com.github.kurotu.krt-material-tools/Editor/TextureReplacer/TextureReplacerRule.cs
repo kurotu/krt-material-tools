c1c69ee381856a55fbb9be4e274e9e8caca496c0
kurotu <kurotu@gmail.com>
2023-10-31 21:15:08 +0900

    Improve TextureReplacerWindow
    - Serialize adhoc rule
    - `Save As...` button

---
+++ Packages/com.github.kurotu.krt-material-tools/Editor/TextureReplacer/TextureReplacerRule.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace KRT.MaterialTools.TextureReplacer
+{
+    [Serializable]
+    internal class TextureReplacerRule
+    {
+        [SerializeField]
+        private List<TexturePair> texturePairs = new List<TexturePair>();
+
+        internal bool IsBlank => texturePairs.Where(pair => pair.replacement != null).Count() == 0;
+
+        internal void SetTexture(Texture key, Texture value)
+        {
+            var index = texturePairs.FindIndex(p => p.original == key);
+            if (index >= 0)
+            {
+                texturePairs[index].replacement = value;
+            }
+            else
+            {
+                texturePairs.Add(new TexturePair { original = key, replacement = value });
+            }
+        }
+
+        internal Texture GetTexture(Texture key)
+        {
+            return texturePairs.FirstOrDefault(p => p.original == key)?.replacement;
+        }
+
+        internal bool ContainsKey(Texture key)
+        {
+            return texturePairs.Any(p => p.original == key);
+        }
+
+        internal Dictionary<Texture, Texture> ToDictionary()
+        {
+            return texturePairs.ToDictionary(p => p.original, p => p.replacement);
+        }
+
+        internal void Clear()
+        {
+            texturePairs.Clear();
+        }
+
+        [Serializable]
+        private class TexturePair
+        {
+            public Texture original;
+            public Texture replacement;
+        }
+    }
+}
--- Packages/com.github.kurotu.krt-material-tools/Editor/TextureReplacer/TextureReplacerWindow.cs
+++ Packages/com.github.kurotu.krt-material-tools/Editor/TextureReplacer/TextureReplacerWindow.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
@@ -9,9 +9,9 @@ namespace KRT.MaterialTools.TextureReplacer
     internal class TextureReplacerWindow : EditorWindow
     {
         internal Material material;
-        internal Dictionary<Texture, Texture> adhocRule = new Dictionary<Texture, Texture>();
 
-        private List<TexturePropertyPair> texturePairs;
+        [SerializeField]
+        private TextureReplacerRule adhocRule = new TextureReplacerRule();
 
         [SerializeField]
         internal Vector2 scrollPosition;
@@ -36,9 +36,9 @@ namespace KRT.MaterialTools.TextureReplacer
                 return;
             }
 
-            texturePairs = GetTextures(material);
-            var textures = texturePairs.Select(p => p.texture).Where(t => t != null).Distinct().ToArray();
-            Dictionary<Texture, string[]> props = textures.ToDictionary(t => t, t => texturePairs.Where(p => p.texture == t).Select(p => p.propertyName).ToArray());
+            var texturePropertyPairs = GetTexturePropertyPairs(material);
+            var textures = texturePropertyPairs.Select(p => p.texture).Where(t => t != null).Distinct().ToArray();
+            Dictionary<Texture, string[]> props = textures.ToDictionary(t => t, t => texturePropertyPairs.Where(p => p.texture == t).Select(p => p.propertyName).ToArray());
 
             using (var scroll = new EditorGUILayout.ScrollViewScope(scrollPosition))
             {
@@ -54,10 +54,10 @@ namespace KRT.MaterialTools.TextureReplacer
 
                         if (!adhocRule.ContainsKey(tex))
                         {
-                            adhocRule[tex] = null; 
+                            adhocRule.SetTexture(tex, null);
                         }
-                        var replacement = adhocRule[tex];
-                        adhocRule[tex] = (Texture)EditorGUILayout.ObjectField(replacement, replacement != null ? replacement.GetType() : typeof(Texture), false, GUILayout.Width(80), GUILayout.Height(80));
+                        var replacement = adhocRule.GetTexture(tex);
+                        adhocRule.SetTexture(tex, (Texture)EditorGUILayout.ObjectField(replacement, replacement != null ? replacement.GetType() : typeof(Texture), false, GUILayout.Width(80), GUILayout.Height(80)));
 
                         using (var vertical = new EditorGUILayout.VerticalScope())
                         {
@@ -72,9 +72,20 @@ namespace KRT.MaterialTools.TextureReplacer
 
             EditorGUILayout.Space();
 
-            if (GUILayout.Button("Apply"))
+            using (var horizontal = new EditorGUILayout.HorizontalScope())
             {
-                OnClickApply();
+                using (var disable = new EditorGUI.DisabledGroupScope(adhocRule.IsBlank))
+                {
+                    if (GUILayout.Button("Apply"))
+                    {
+                        OnClickApply();
+                    }
+
+                    if (GUILayout.Button("Save As..."))
+                    {
+                        OnClickSaveAsNewMaterial();
+                    }
+                }
             }
 
             EditorGUILayout.Space();
@@ -82,20 +93,30 @@ namespace KRT.MaterialTools.TextureReplacer
 
         private void OnClickApply()
         {
-            adhocRule.Where(p => p.Value != null).ToList().ForEach(p =>
-            {
-                foreach (var prop in texturePairs.Where(tp => tp.texture == p.Key))
-                {
-                    material.SetTexture(prop.propertyName, p.Value);
-                }
-            });
+            ApplyTextures(material, adhocRule);
+            adhocRule.Clear();
         }
 
-        private List<TexturePropertyPair> GetTextures(Material material)
+        private void OnClickSaveAsNewMaterial()
+        {
+            var path = AssetDatabase.GetAssetPath(material);
+            var newPath = AssetDatabase.GenerateUniqueAssetPath(path);
+            newPath = EditorUtility.SaveFilePanelInProject("Save as New Material", Path.GetFileNameWithoutExtension(newPath), "mat", "", Application.dataPath);
+            if (newPath == null)
+            {
+                return;
+            }
+            var newMat = new Material(material);
+            ApplyTextures(newMat, adhocRule);
+            AssetDatabase.CreateAsset(newMat, newPath);
+            adhocRule.Clear();
+        }
+
+        private List<TexturePropertyPair> GetTexturePropertyPairs(Material material)
         {
             var list = new List<TexturePropertyPair>();
             var propNames = material.GetTexturePropertyNames();
-            foreach(var prop in propNames)
+            foreach (var prop in propNames)
             {
                 var tex = material.GetTexture(prop);
                 list.Add(new TexturePropertyPair { propertyName = prop, texture = tex });
@@ -103,11 +124,18 @@ namespace KRT.MaterialTools.TextureReplacer
             return list;
         }
 
-        [Serializable]
-        internal class TexturePair
+        private void ApplyTextures(Material mat, TextureReplacerRule rule)
         {
-            public Texture original;
-            public Texture replacement;
+            Undo.RecordObject(mat, "Apply Textures");
+            var texturePropertyPairs = GetTexturePropertyPairs(mat);
+            foreach (var pair in rule.ToDictionary().Where(p => p.Value != null))
+            {
+                foreach (var prop in texturePropertyPairs.Where(tp => tp.texture == pair.Key))
+                {
+                    mat.SetTexture(prop.propertyName, pair.Value);
+                }
+            }
+            Undo.FlushUndoRecordObjects();
         }
 
         private class TexturePropertyPair
