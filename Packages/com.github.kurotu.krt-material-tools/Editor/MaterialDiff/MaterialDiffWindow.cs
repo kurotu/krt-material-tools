88f21653fd7d8dc3a484f9656e3b0e20194b8f5f
kurotu <kurotu@gmail.com>
2024-09-19 01:45:27 +0900

    Add button events

--- Packages/com.github.kurotu.krt-material-tools/Editor/MaterialDiff/MaterialDiffWindow.cs
+++ Packages/com.github.kurotu.krt-material-tools/Editor/MaterialDiff/MaterialDiffWindow.cs
@@ -38,11 +38,9 @@ namespace KRT.MaterialTools.MaterialDiff
             {
                 EditorGUILayout.LabelField("Materials");
                 leftMat = (Material)EditorGUILayout.ObjectField(leftMat, typeof(Material), false);
-                if (GUILayout.Button("Ì"))
+                if (GUILayout.Button(new GUIContent("Ì", "Swap left and right")))
                 {
-                    var tmp = leftMat;
-                    leftMat = rightMat;
-                    rightMat = tmp;
+                    OnClickSwap();
                 }
                 rightMat = (Material)EditorGUILayout.ObjectField(rightMat, typeof(Material), false);
             }
@@ -76,11 +74,11 @@ namespace KRT.MaterialTools.MaterialDiff
                             }
                             if (GUILayout.Button(new GUIContent("¨", "Copy to right")))
                             {
-
+                                OnClickCopyToRight(propName, leftValue);
                             }
                             if (GUILayout.Button(new GUIContent("©", "Copy to left")))
                             {
-
+                                OnClickCopyToLeft(propName, rightValue);
                             }
                             using (new EditorGUI.DisabledScope(true))
                             {
@@ -96,6 +94,23 @@ namespace KRT.MaterialTools.MaterialDiff
                     EditorGUILayout.EndFoldoutHeaderGroup();
                 }
             }
+        }
+
+        private void OnClickCopyToLeft(string propName, MaterialPropertyValue rightValue)
+        {
+            SetPropertyValue(leftMat, propName, rightValue);
+        }
+
+        private void OnClickCopyToRight(string propName, MaterialPropertyValue leftValue)
+        {
+            SetPropertyValue(rightMat, propName, leftValue);
+        }
+
+        private void OnClickSwap()
+        {
+            var tmp = leftMat;
+            leftMat = rightMat;
+            rightMat = tmp;
         }
 
         private void DrawPropertyValueField(MaterialPropertyValue value)
@@ -168,6 +183,31 @@ namespace KRT.MaterialTools.MaterialDiff
                 propNames[i] = ShaderUtil.GetPropertyName(s, i);
             }
             return propNames.Where((n, i) => ShaderUtil.GetPropertyType(s, i) == type).ToArray();
+        }
+
+        private void SetPropertyValue(Material mat, string name, MaterialPropertyValue value)
+        {
+            switch (value.Type)
+            {
+                case ShaderUtil.ShaderPropertyType.Color:
+                    mat.SetColor(name, value.GetColor());
+                    break;
+                case ShaderUtil.ShaderPropertyType.Vector:
+                    mat.SetVector(name, value.GetVector());
+                    break;
+                case ShaderUtil.ShaderPropertyType.Float:
+                    mat.SetFloat(name, value.GetFloat());
+                    break;
+                case ShaderUtil.ShaderPropertyType.Range:
+                    mat.SetFloat(name, value.GetRange());
+                    break;
+                case ShaderUtil.ShaderPropertyType.TexEnv:
+                    mat.SetTexture(name, value.GetTexture());
+                    break;
+                case ShaderUtil.ShaderPropertyType.Int:
+                    mat.SetInt(name, value.GetInt());
+                    break;
+            }
         }
     }
 }
